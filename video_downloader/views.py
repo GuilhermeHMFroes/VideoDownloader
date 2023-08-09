468490ad87af505a7a1a43243549d797ed8ba8e7
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2023-08-09 15:10:04 -0300

    initial commit: Setup Project

---
+++ video_downloader/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    """ View Index.
+        Exibe o arquivo localizado em `/config/templates/index.html`
+    """
+    # abre o arquivo `README.md` e le o conteudo do readme
+    # with open(os.path.join(BASE_DIR, 'README.md'), 'r', encoding='UTF-8') as readme_file:
+    #     readme_content = readme_file.read()
+
+    # converte o conteudo em markdown para html
+    # index_content = markdown(readme_content)
+
+    # context = {
+    #     'index_content': index_content,
+    # }
+    context = {}
+
+    return render(request, 'index.html', context)
